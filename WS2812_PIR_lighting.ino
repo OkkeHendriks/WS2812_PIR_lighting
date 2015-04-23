2ff4f12725a9d0e4bb325b90ab7a00a2f33143c1
Okke Hendriks <hendriks@robot-rose.nl>
2015-04-23 23:58:58 +0200

    Cleanup and events
    Added after event using timer library and cleanup

--- WS2812_PIR_lighting.ino
+++ WS2812_PIR_lighting.ino
@@ -6,7 +6,7 @@
 #define DELAY 10
 
 #define SENSOR_PIN 8
-#define DEBUG_PIN 2
+#define DEBUG_LED_PIN 13
 
 #define ON_TIME 2500     // [ms]
 
@@ -24,79 +24,58 @@ unsigned int    target_brightness   = 0;
 CRGB            target_color        = CRGB::Beige;
 unsigned int    fade_speed          = 50;
 STATE           timer_state         = ON;
+int             off_event           = 0;
 
 CRGB leds[LED_COUNT];                        // Led array
 
 void setup()
 {
   FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, LED_COUNT);
-  pinMode(SENSOR_PIN, INPUT);      // sets the digital pin 7 as input
-  pinMode(DEBUG_PIN, OUTPUT);      // sets the  pin 7 as input
+  pinMode(SENSOR_PIN, INPUT);      // sets the SENSOR_PIN as input
+  pinMode(DEBUG_LED_PIN, OUTPUT);  // sets the DEBUG_LED_PIN as output
 
-  clearLEDs();   // This function, defined below, turns all LEDs off...
-  showLEDs();
-  delay(500);
-  allLedsWhite();
-  showLEDs();
-  delay(500);
-  clearLEDs();   // This function, defined below, turns all LEDs off...
-  showLEDs();
-  delay(500);
   t.every(1, process_fade);
+  
   set_fade_target(CRGB::Beige, 128, 10);
 
-  blink_debug(6);
+  blink_debug(3, 250);
+}
+
+void start_event(int& event, int time, void (* callback)())
+{
+  stop_event(event);
+  event = t.after(time, callback);
+}
+
+void stop_event(int& event)
+{
+  if ( event > 0 )
+    t.stop(event);
 }
 
 void loop()
 {
   if(check_sensor())
   {
+    debug_on();
     set_fade_target(CRGB::Beige, 255, 20);
-    
-    // Reset and turn on timer
-    timer = 0;
-    timer_state = ON;
+    start_event(off_event, ON_TIME, turn_off);      
   }
-  
-  if(timer_state == ON)
-     digitalWrite(DEBUG_PIN, HIGH); 
   else
-     digitalWrite(DEBUG_PIN, LOW); 
+    debug_off();
   
-  // If timer > ON_TIME
-  if(timer > ON_TIME)
-  {
-    set_fade_target(CRGB::Beige, 0, 20);
-    
-    // Reset and turn off timer
-    timer = 0;
-    timer_state = OFF; 
-  }   
-  
-  if(timer_state == ON)
-    timer++;
-
   t.update();     
+  delay(1);
+}
 
-  delay(1)  ;
-
+void turn_off()
+{
+  set_fade_target(CRGB::Beige, 0, 20);
 }
 
 bool check_sensor()
 {
   return digitalRead(SENSOR_PIN) == HIGH;
-}
-
-void blink_debug(byte count)
-{
-  for(int i = 0; i < 5; i++)
-  {
-    digitalWrite(DEBUG_PIN, HIGH);
-    delay(250);
-    digitalWrite(DEBUG_PIN, LOW);
-    delay(250);
-  }
 }
 
 void set_fade_target(CRGB color, int to_brightness, byte wait)
@@ -156,3 +135,25 @@ void showLEDs()
 {
     FastLED.show();       // Display LEDs 
 }
+
+void debug_on()
+{
+  digitalWrite(DEBUG_LED_PIN, HIGH); 
+}
+
+void debug_off()
+{
+  digitalWrite(DEBUG_LED_PIN, LOW);
+}
+
+void blink_debug(byte count, unsigned int pause)
+{
+  for(int i = 0; i < count; i++)
+  {
+    debug_on();
+    delay(pause);
+    debug_off();
+    delay(pause);
+  }
+}
+
