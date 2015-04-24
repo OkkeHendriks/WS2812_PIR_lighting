f7037737b1cb14fda4f3471b259c0e839372aec9
Okke Hendriks <hendriks@robot-rose.nl>
2015-04-24 03:14:59 +0200

    Added blend
    Just broken blend

--- WS2812_PIR_lighting.ino
+++ WS2812_PIR_lighting.ino
@@ -8,7 +8,7 @@
 #define SENSOR_PIN 8
 #define DEBUG_LED_PIN 13
 
-#define ON_TIME 2500     // [ms]
+#define ON_TIME 20000     // [ms]
 
 enum STATE {
   ON,
@@ -17,17 +17,29 @@ enum STATE {
 
 Timer           t;
 int             sensor_activated    = 0;
-unsigned long   timer               = 0;
-unsigned long   fade_timer          = 0;
-unsigned int    current_brightness  = 0;
-unsigned int    target_brightness   = 0;
+byte            current_brightness  = 0;
+byte            target_brightness   = 0;
+int             current_blend_progress = 0;
 CRGB            target_color        = CRGB::Beige;
 unsigned int    fade_speed          = 50;
+unsigned int    blend_speed         = 50;
 STATE           timer_state         = ON;
 int             off_event           = 0;
 int             fade_event          = 0;
+int             blend_event         = 0;
+int             red_event           = 0;
+int             green_event         = 0;
 
-CRGB leds[LED_COUNT];                        // Led array
+CRGB leds[LED_COUNT];
+byte blend_start_r[LED_COUNT];
+byte blend_start_g[LED_COUNT];
+byte blend_start_b[LED_COUNT];
+byte blend_current_r[LED_COUNT];
+byte blend_current_g[LED_COUNT];
+byte blend_current_b[LED_COUNT];
+byte blend_target_r[LED_COUNT];
+byte blend_target_g[LED_COUNT];
+byte blend_target_b[LED_COUNT];
 
 // Deleting if one already started on that integer (id)
 void start_after_event(int& event, unsigned int timeout, void (* callback)())
@@ -37,10 +49,10 @@ void start_after_event(int& event, unsigned int timeout, void (* callback)())
 }
 
 // Deleting if one already started on that integer (id)
-void start_every_event(int& event, void (* callback)())
+void start_every_event(int& event, unsigned int period, void (* callback)())
 {
    stop_event(event);
-   event = t.every(event, callback);
+   event = t.every(period, callback);
 }
 
 // Stop an event if active
@@ -53,34 +65,61 @@ void stop_event(int& event)
 
 void setup()
 {
-  FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, LED_COUNT);
+  Serial.begin(115200);
+  Serial.flush();
+  
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, LED_COUNT);
   pinMode(SENSOR_PIN, INPUT);      // sets the SENSOR_PIN as input
   pinMode(DEBUG_LED_PIN, OUTPUT);  // sets the DEBUG_LED_PIN as output
 
-  start_every_event(fade_event, process_fade);
-  set_fade_target(CRGB::Beige, 128, 10);
-
+  setLEDs(leds, CRGB::Black);
+  showLEDs();
+  
+  set_fade(0, 5);
+  set_blend(CRGB::White, 5);
+  
   blink_debug(5, 50);
 }
 
+void set_red()
+{
+  blink_debug(5, 50);
+  stop_event(red_event);
+  set_blend(CRGB::Red, 50);
+}
+
+void set_green()
+{
+  blink_debug(5, 150);
+  stop_event(green_event);
+  set_blend(CRGB::Green, 50);
+}
+
+
 void loop()
 {
-  if(check_sensor())
+  bool sensor = check_sensor();
+  if(sensor)
   {
     debug_on();
-    set_fade_target(CRGB::Beige, 255, 20);
+    set_fade(255, 20);
+    set_blend(CRGB::Blue, 50);
+    
+    start_after_event(red_event, 2000, set_red);
+    start_after_event(green_event, 5000, set_green);
+    
     start_after_event(off_event, ON_TIME, turn_off);      
   }
   else
     debug_off();
   
   t.update();     
-  delay(1);
 }
 
 void turn_off()
 {
-  set_fade_target(CRGB::Beige, 0, 20);
+  stop_event(off_event);
+  set_fade(0, 20);
 }
 
 bool check_sensor()
@@ -88,38 +127,99 @@ bool check_sensor()
   return digitalRead(SENSOR_PIN) == HIGH;
 }
 
-void set_fade_target(CRGB color, int to_brightness, byte wait)
+void set_fade( byte to_brightness, unsigned int fade_speed)
 {
-  target_color      = color;
   target_brightness = to_brightness;
-  fade_speed        = wait;
+  start_every_event(fade_event, fade_speed, process_fade);
+}
+
+void set_blend(CRGB target_color, unsigned int blend_speed)
+{
+  for ( int i = 0; i < LED_COUNT; ++i)
+  {
+    blend_current_r[i] = 0;
+    blend_current_g[i] = 0;
+    blend_current_b[i] = 0;
+    blend_start_r[i] = leds[i].r;
+    blend_start_g[i] = leds[i].g;
+    blend_start_b[i] = leds[i].b;
+    blend_target_r[i] = target_color.r;
+    blend_target_g[i] = target_color.g;
+    blend_target_b[i] = target_color.b;
+  }
+  current_blend_progress = 0;
+  start_every_event(blend_event, blend_speed, process_blend);
 }
   
 void process_fade()
 {  
   if (current_brightness == target_brightness)
     return;
-    
-  if(fade_timer > 0)
-  {
-    // Timer did not yet expire
-    fade_timer--;
-    return;      
-  }
+  
+  if(current_brightness < target_brightness)
+    current_brightness++;
   else
+    current_brightness--;     
+  
+  apply_blend(); 
+  for ( int i = 0; i < LED_COUNT; ++i )
   {
-    // Reset fade timer
-    fade_timer = fade_speed;  
+    leds[i].nscale8( current_brightness );
+  }
+  showLEDs();
+}
+
+void apply_blend()
+{
+  for ( int i = 0; i < LED_COUNT; ++i )
+  {
+    leds[i].r = blend_current_r[i];
+    leds[i].g = blend_current_g[i];
+    leds[i].b = blend_current_b[i];
+  }
+}
+
+void process_blend()
+{
+//  Serial.print("current_blend_progress " );
+//  Serial.println(current_blend_progress);
+//  
+//  Serial.print("blend_start:  " );
+//  Serial.print(blend_start_r[0]);
+//  Serial.print("|" );
+//  Serial.print(blend_start_g[0]);
+//  Serial.print("|" );
+//  Serial.print(blend_start_b[0]);
+//  Serial.println("");
+//  
+//  Serial.print("blend_target:  " );
+//  Serial.print(blend_target_r[0]);
+//  Serial.print("|" );
+//  Serial.print(blend_target_g[0]);
+//  Serial.print("|" );
+//  Serial.print(blend_target_b[0]);
+//  Serial.println("");
+//  
+//  Serial.print("blend_current: ");
+//  Serial.print(blend_current_r[0]);
+//  Serial.print("|" );
+//  Serial.print(blend_current_g[0]);
+//  Serial.print("|" );
+//  Serial.print(blend_current_b[0]);
+//  Serial.println("");
+  
+  if (current_blend_progress == 255)
+    return;
     
-    if(current_brightness < target_brightness)
-      current_brightness++;
-    else
-      current_brightness--;     
-    
-    setLEDs(target_color);
-    for (int i = 0; i < LED_COUNT; i++)
-      leds[i].nscale8( current_brightness );
-    showLEDs();
+  current_blend_progress++;
+  for ( int i = 0; i < LED_COUNT; ++i)
+  {
+    int r = (int)blend_target_r[i] - (int)blend_start_r[i];
+    int g = (int)blend_target_g[i] - (int)blend_start_g[i];
+    int b = (int)blend_target_b[i] - (int)blend_start_b[i];
+    blend_current_r[i] = blend_start_r[i] + (r*current_blend_progress)/255;
+    blend_current_g[i] = blend_start_g[i] + (g*current_blend_progress)/255;
+    blend_current_b[i] = blend_start_b[i] + (b*current_blend_progress)/255; 
   }
 }
 
@@ -135,10 +235,15 @@ void allLedsWhite()
     leds[i] = CRGB::White;
 }
 
-void setLEDs(CRGB color)
+void setLED(CRGB* set_leds, unsigned int index, CRGB color)
 {
-  for (int i = 0; i < LED_COUNT; i++)
-    leds[i] = color;
+  set_leds[index] = color;
+}
+
+void setLEDs(CRGB* set_leds, CRGB color)
+{
+  for ( int i = 0; i < LED_COUNT; ++i )
+    set_leds[i] = color;
 }
 
 void showLEDs()
@@ -158,7 +263,7 @@ void debug_off()
 
 void blink_debug(byte count, unsigned int pause)
 {
-  for(int i = 0; i < count; i++)
+  for( int i = 0; i < count; ++i )
   {
     debug_on();
     delay(pause);
